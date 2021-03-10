40b28c6336c838b2ce67519bbf0d23f28440636d
Joe Pea <joe@trusktr.io>
2021-03-10 12:33:12 -0800

    initial Promise PoC: very basic, missing features, it just shows a '.then' callback handling the result of a 'setTimeout' inside a 'new Promise' where 'resolve' is finally called once the timeout executes.

--- assembly/Promise.ts
+++ assembly/Promise.ts
@@ -9,8 +9,9 @@ type Executor<T> = (resRej: PromiseActions<T>) => void
 // TODO Use the Error class instead of string. Keeping it simple for the PoC for now.
 type Err = string
 
-class PromiseActions<T> {
-	constructor(private promise: Promise<T>) {}
+// We shouldn't have to export this (or this class should even exist) once AS supports closures.
+export class PromiseActions<T> {
+	/*friend*/ constructor(private promise: Promise<T>) {}
 
 	resolve(result: T): void {
 		if (this.promise.__isSettled) return
@@ -54,44 +55,39 @@ export class Promise<T> {
 	__thenCallback: Array<(val: T) => void> = []
 
 	then(cb: (v: T) => void): void {
-		// If there's already a callback
-		if (this.__thenCallback.length) throw new Error('then/catch chaining not supported yet.')
+		if (this.__thenCallback.length) throw new Error('Promise chaining or multiple then/catch calls not yet supported.')
 
 		this.__thenCallback.push(cb)
 
-		if (this.__result.length) {
-			// The goal here is to run the callback in the next microtask, as per Promise spec.
-
-			// XXX unable to pass method pointers:
-			// defer<(this: Promise<T>) => void>(this.__runThen)
-			// _defer(ptr(this.__runThen))
-			// _defer(ptr<(this: Promise<T>) => void>(this.__runThen))
-
-			deferWithArg((selfPtr: usize) => {
-				const self = changetype<Promise<T>>(selfPtr)
-				self.__runThen()
-			}, ptr(this))
-		}
+		if (this.__result.length) this.__runThen()
 	}
 
 	__runThen(): void {
-		if (!this.__thenCallback.length || !this.__result.length) throw new Error('This should not happen.')
-		const fn = this.__thenCallback[0]
-		fn(this.__result[0])
+		// XXX unable to pass methods as callbacks:
+		// defer<(this: Promise<T>) => void>(this.anyMethod)
+		// _defer(ptr(this.anyMethod))
+		// _defer(ptr<(this: Promise<T>) => void>(this.anyMethod))
+
+		// The goal here is to run the callback in the next microtask, as per Promise spec.
+		deferWithArg((selfPtr: usize) => {
+			const self = changetype<Promise<T>>(selfPtr)
+
+			if (!self.__thenCallback.length || !self.__result.length) throw new Error('This should not happen.')
+
+			const fn = self.__thenCallback[0]
+
+			fn(self.__result[0])
+		}, ptr(this))
 	}
 
 	__catchCallback: Array<(err: Err) => void> = []
 
 	catch(cb: (err: Err) => void): void {
-		// If there's already a callback
-		if (this.__catchCallback.length) throw new Error('then/catch chaining not supported yet.')
+		if (this.__catchCallback.length) throw new Error('Promise chaining or multiple then/catch calls not yet supported.')
 
 		this.__catchCallback.push(cb)
 
-		if (this.__error.length) {
-			// TODO defer
-			this.__runCatch()
-		}
+		if (this.__error.length) this.__runCatch()
 	}
 
 	__runCatch(): void {
--- assembly/index.ts
+++ assembly/index.ts
@@ -1,6 +1,12 @@
-import {Promise} from './Promise'
+import {Promise, PromiseActions} from './Promise'
 import {setTimeout} from './setTimeout'
 import {logf32} from './logf32'
+
+// User should not new PromiseActions directly, but we have to export it so
+// they can create a variable that will reference it because there are no
+// closures. Once closures land, this class will not be exported, and the end
+// user will use callbacks passed into new Promise executors.
+let actions: PromiseActions<i32> | null = null
 
 function test(): void {
 	setTimeout(() => {
@@ -9,11 +15,20 @@ function test(): void {
 
 	logf32(2.0)
 
-	const p = new Promise<i32>(actions => {
+	const p = new Promise<i32>(_actions => {
+		actions = _actions
+
 		logf32(3.0)
-		// setTimeout(() => {
-		actions.resolve(10)
-		// }, 2000)
+
+		setTimeout(() => {
+			// We will not need any null assertion when closures allows us to
+			// remove PromiseActions and therefore the user relies on the
+			// resolve/reject functions that will be passed into here, but for
+			// now they rely on actions object being passed in, and there's no
+			// way to reference it inside the setTimeout callback except for
+			// storing it on a global variable due to lacking closures.
+			actions!.resolve(10)
+		}, 2000)
 	})
 
 	p.then((n: i32) => {
