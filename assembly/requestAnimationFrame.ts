3ae8d4951aae1384c889739ad710a9c6d5f6cbe4
Joe Pea <joe@trusktr.io>
2021-07-02 00:57:38 -0700

    return animation frame ID from rAF

--- assembly/requestAnimationFrame.ts
+++ assembly/requestAnimationFrame.ts
@@ -1,12 +1,12 @@
 import {fnPointerToIndex, ptr} from './utils'
 
-declare function _requestAnimationFrame(fn: usize): void
+declare function _requestAnimationFrame(fn: usize): i32
 
 export {_requestAnimationFrame}
 
-export function requestAnimationFrame<T>(fn: T): void {
+export function requestAnimationFrame<T>(fn: T): i32 {
 	if (!isFunction<T>(fn)) throw new Error('Must pass a function of type `() => void`.')
-	_requestAnimationFrame(fnPointerToIndex(ptr(fn)))
+	return _requestAnimationFrame(fnPointerToIndex(ptr(fn)))
 }
 
 export declare function cancelAnimationFrame(id: i32): void
